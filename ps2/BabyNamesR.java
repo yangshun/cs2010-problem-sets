bec4afa11ad965b53e8974e62baca13969206d6a
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-09-06 02:10:51 +0800

    [PS2] Use suffix array

--- ps2/BabyNamesR.java
+++ ps2/BabyNamesR.java
@@ -7,85 +7,59 @@ import java.io.*;
 // write list of collaborators here:
 // year 2015 hash code: JESg5svjYpIsmHmIjabX (do NOT delete this line)
 
-class Node {
-  public TreeMap<Character, Node> neighbours;
-  public TreeSet<String> words;
-
-  public Node() {
-    neighbours = new TreeMap<Character, Node>();
-    words = new TreeSet<String>();
-  }
-
-  public String neighboursAsString() {
-    Set<Character> set = neighbours.keySet();
-    if (set == null) {
-      return "[]";
-    }
-    String ns = Arrays.toString(set.toArray());
-    return ns == null ? "[]" : ns;
-  }
-
-  public String wordsAsString() {
-    String ns = Arrays.toString(words.toArray());
-    return ns == null ? "[]" : ns;
-  }
-}
-
-class Trie {
-  private Node root;
-
-  public Trie() {
-    root = new Node();
-  }
-
-  private void insertWord(Node node, String word, String originalWord) {
-    node.words.add(originalWord);
-    Character c = word.charAt(0);
-    Node nextNode = node.neighbours.get(c);
-    if (nextNode == null) {
-      nextNode = new Node();
-      node.neighbours.put(c, nextNode);
-    }
-    nextNode.words.add(originalWord);
-    if (word.length() > 1) {
-      insertWord(nextNode, word.substring(1), originalWord);
-    }
-  }
-
-  public void insertWord(String word) {
-    for (int i = 0; i < word.length(); i++) {
-      insertWord(root, word.substring(i), word);
-    }
-  }
-
-  public int findWord(String word) {
-    Node node = root;
-    for (int i = 0; i < word.length(); i++) {
-      Character c = word.charAt(i);
-      node = node.neighbours.get(c);
-      if (node == null && i != word.length() - 1) {
-        return 0;
-      }
-    }
-    return node.words.size();
-  }
-}
-
 class BabyNamesR {
   // if needed, declare a private data structure here that
   // is accessible to all methods in this class
-  Trie names;
+  public TreeMap<String, String[]> nameSuffixes;
 
   public BabyNamesR() {
-    names = new Trie();
+    nameSuffixes = new TreeMap<String, String[]>();
   }
 
   void AddSuggestion(String babyName) {
-    names.insertWord(babyName);
+    int N = babyName.length();
+    String[] suffixes = new String[N];
+    for (int i = 0; i < N; i++) {
+      suffixes[i] = babyName.substring(i, N);
+    }
+    Arrays.sort(suffixes);
+    nameSuffixes.put(babyName, suffixes);
+  }
+
+  boolean countOccurences(String pattern, String[] suffixes, String key) {
+    int l = 0;
+    int r = suffixes.length;
+    while (l < r) {
+      int mid = (l+r) / 2;
+      if (pattern.compareTo(suffixes[mid]) > 0) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
+    }
+    int s = l;
+    r = suffixes.length;
+    while (l < r) {
+      int mid = (l+r) / 2;
+      if (pattern.compareTo(suffixes[mid]) < 0) {
+        r = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return s != r ? true : suffixes[Math.min(suffixes.length - 1, s)].contains(pattern);
   }
 
   int Query(String SUBSTR) {
-    return names.findWord(SUBSTR);
+    int count = 0;
+    for (Map.Entry<String, String[]> entry : nameSuffixes.entrySet()) {
+      String key = entry.getKey();
+      Object value = entry.getValue();
+      if (countOccurences(SUBSTR, (String[])value, key)) {
+        count += 1;
+      }
+    }
+    return count;
   }
 
   void run() throws Exception {
