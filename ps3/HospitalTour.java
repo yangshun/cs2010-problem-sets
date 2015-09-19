b6b2dc05b8655e7d1abf6686b6365d88dbaf18c7
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-09-19 18:38:36 +0800

    [PS3] Passed subtask C

--- ps3/HospitalTour.java
+++ ps3/HospitalTour.java
@@ -10,71 +10,74 @@ import java.lang.*;
 
 class HospitalTour {
   private int V;
-  private TreeMap<Integer, TreeSet<Integer> > AdjMatrix;
+  private TreeMap<Integer, TreeMap<Integer, Boolean> > AdjMatrix;
   private int[] RatingScore;
 
-  private TreeSet<Integer> visited;
+  private TreeMap<Integer, Boolean> visited;
   private TreeMap<Integer, Integer> depth;
   private TreeMap<Integer, Integer> low;
   private TreeMap<Integer, Integer> parent;
-  private TreeSet<Integer> articulationPoints;
-
-  public HospitalTour() {
-    // Write necessary code during construction
-    //
-    // write your answer here
-  }
+  private TreeMap<Integer, Boolean> articulationPoints;
 
   void getArticulationPoints(int i, int d) {
-    visited.add(i);
+    visited.put(i, true);
     depth.put(i, d);
     low.put(i, d);
     int childCount = 0;
     boolean isArticulation = false;
 
-    Iterator itr = AdjMatrix.get(i).iterator();
+    for (Map.Entry<Integer, Boolean> entry : AdjMatrix.get(i).entrySet()) {
+      int ni = (int)entry.getKey();
 
-    while (itr.hasNext()) {
-      int ni = (int)itr.next();
-      if (!visited.contains(ni)) {
+      if (visited.get(ni) == null) {
         parent.put(ni, i);
         getArticulationPoints(ni, d + 1);
         childCount++;
-        if (low.get(ni) >= depth.get(i)) {
+        if (low.get(ni) != null && depth.get(i) != null && (low.get(ni) >= depth.get(i))) {
           isArticulation = true;
         }
-        low.put(i, Math.min(low.get(i), low.get(ni)));
-      } else if (ni != parent.get(i)) {
-        low.put(i, Math.min(low.get(i), depth.get(ni)));
+        if (low.get(i) != null && low.get(ni) != null) {
+          low.put(i, Math.min(low.get(i), low.get(ni)));
+        } else if (low.get(i) == null && low.get(ni) != null) {
+          low.put(i, low.get(ni));
+        } else if (low.get(i) != null && low.get(ni) == null) {
+          low.put(i, low.get(i));
+        }
+      } else if (parent.get(i) == null || ni != parent.get(i)) {
+        if (low.get(i) != null && depth.get(ni) != null) {
+          low.put(i, Math.min(low.get(i), depth.get(ni)));
+        } else if (low.get(i) == null && depth.get(ni) != null) {
+          low.put(i, depth.get(ni));
+        } else if (low.get(i) != null && depth.get(ni) == null) {
+          low.put(i, low.get(i));
+        }
       }
     }
 
     if ((parent.get(i) != null && isArticulation) || (parent.get(i) == null && childCount > 1)) {
-      articulationPoints.add(i);
+      articulationPoints.put(i, true);
     }
   }
 
   int Query() {
     int minimum = 100001;
 
-    visited = new TreeSet<Integer>();
+    visited = new TreeMap<Integer, Boolean>();
     depth = new TreeMap<Integer, Integer>();
     low = new TreeMap<Integer, Integer>();
     parent = new TreeMap<Integer, Integer>();
-    articulationPoints = new TreeSet<Integer>();
+    articulationPoints = new TreeMap<Integer, Boolean>();
 
     getArticulationPoints(0, 0);
 
-    Iterator itr = articulationPoints.iterator();
-
-    while (itr.hasNext()) {
-      int i = (int)itr.next();
+    for (Map.Entry<Integer, Boolean> entry : articulationPoints.entrySet()) {
+      int i = (int)entry.getKey();
       if (RatingScore[i] < minimum) {
         minimum = RatingScore[i];
       }
     }
 
-    return minimum;
+    return minimum != 100001 ? minimum : -1;
   }
 
   void run() throws Exception {
@@ -95,28 +98,28 @@ class HospitalTour {
       }
 
       // clear the graph and read in a new graph as Adjacency Matrix
-      AdjMatrix = new TreeMap<Integer, TreeSet<Integer> >();
+      AdjMatrix = new TreeMap<Integer, TreeMap<Integer, Boolean> >();
       for (int i = 0; i < V; i++) {
         st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
 
-        TreeSet<Integer> g = AdjMatrix.get(i);
+        TreeMap<Integer, Boolean> g = AdjMatrix.get(i);
         if (g == null) {
-          g = new TreeSet<Integer>();
+          g = new TreeMap<Integer, Boolean>();
           AdjMatrix.put(i, g);
         }
 
         while (k-- > 0) {
           int j = Integer.parseInt(st.nextToken());
 
-          g.add(j);
-          TreeSet<Integer> gCon = AdjMatrix.get(j);
+          g.put(j, true);
+          TreeMap<Integer, Boolean> gCon = AdjMatrix.get(j);
           if (gCon == null) {
-            gCon = new TreeSet<Integer>();
+            gCon = new TreeMap<Integer, Boolean>();
             AdjMatrix.put(j, gCon);
           }
 
-          gCon.add(i);
+          gCon.put(i, true);
         }
 
       }
